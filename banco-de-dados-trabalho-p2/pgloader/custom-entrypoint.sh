7472039b570d85d84568daaf8a43cdf46369393d
Nicollas-Leal <133714819+Nicollas-Leal@users.noreply.github.com>
2025-01-27 13:36:46 -0300

    Delete banco-de-dados-trabalho-p2 directory

--- banco-de-dados-trabalho-p2/pgloader/custom-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Aguardando mais 10 segundos para garantir que o PostgreSQL foi iniciado completamente"
-sleep 10
-
-echo "Iniciando migração com pgloader..."
-pgloader /app/db.load
-
-exec "$@"
